7c45b88c81b994a285900c73b0c45512b788184a
Zablon Dawit <zabjd22@gmail.com>
2021-11-22 16:32:44 +0300

    Admin can now delete user food record.

--- client/pages/admin/users/[userId]/records/[recordId]/delete.js
+++ client/pages/admin/users/[userId]/records/[recordId]/delete.js
@@ -1,3 +1,96 @@
-const DeleteUserRecordPage = () => <div>Delete Page</div>
+import {
+  DeleteTwoTone,
+  InfoCircleTwoTone,
+  StopTwoTone,
+} from '@ant-design/icons'
+import { Button, Card, Space } from 'antd'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import commonStyles from '../../../../../styles/common.module.css'
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+const DeleteUserRecordPage = () => {
+  const router = useRouter()
+  const { userId, recordId } = router.query
+
+  const [loading, setLoading] = useState(true)
+  const [deleteLoading, setDeleteLoading] = useState(false)
+  const [recordInfo, setRecordInfo] = useState({
+    id: '',
+    name: '',
+    user: {
+      username: '',
+    },
+  })
+
+  const handleDelete = async () => {
+    if ([userId, recordId].map((value) => typeof value).includes('undefined'))
+      return
+
+    try {
+      setDeleteLoading(true)
+      await axios.delete(`/admin/${recordId}`)
+
+      setDeleteLoading(false)
+      router.push(`/admin/users/${userId}`)
+    } catch (err) {
+      console.error('Failed To Delete User Record', {
+        err,
+      })
+    }
+  }
+
+  useEffect(async () => {
+    if ([userId, recordId].map((val) => typeof val).includes('undefined'))
+      return
+
+    try {
+      const response = await axios.get(`/admin/${recordId}`)
+
+      setRecordInfo(response.data.record)
+      setLoading(false)
+    } catch (err) {
+      console.error('Fetching Record Informatino Failed', {
+        err,
+      })
+    }
+  }, [userId, recordId])
+
+  if (loading)
+    return (
+      <div className={commonStyles.container}>
+        <InfoCircleTwoTone /> Loading please wait...
+      </div>
+    )
+
+  return (
+    <div className={commonStyles.container}>
+      <Card>
+        <Title level={4}>Are you sure you want to delete record?</Title>
+
+        <p>
+          Are you sure you want to delete record of <b>{recordInfo.name}</b>,
+          created by <b>{recordInfo.user.username}</b>?
+        </p>
+
+        <Space>
+          <Button
+            loading={deleteLoading}
+            onClick={handleDelete}
+            icon={<DeleteTwoTone />}
+          >
+            Delete
+          </Button>
+          <Button onClick={() => router.back()} icon={<StopTwoTone />}>
+            Cancel
+          </Button>
+        </Space>
+      </Card>
+    </div>
+  )
+}
 
 export default DeleteUserRecordPage
--- client/pages/admin/users/[userId]/records/[recordId]/index.js
+++ client/pages/admin/users/[userId]/records/[recordId]/index.js
@@ -1,3 +1,114 @@
-const AdminUserRecordPage = () => <div>Admin Record Page</div>
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
-export default AdminUserRecordPage
+import commonStyles from '../../../../../styles/common.module.css'
+import { Button, Card, Space, Typography } from 'antd'
+import axios from 'axios'
+import {
+  DeleteTwoTone,
+  EditTwoTone,
+  FolderOpenTwoTone,
+  InfoCircleTwoTone,
+  UserOutlined,
+} from '@ant-design/icons'
+
+const { Title } = Typography
+
+const AdminRecordPage = () => {
+  const router = useRouter()
+  const { recordId } = router.query
+  const [loading, setLoading] = useState(true)
+  const [recordInfo, setRecordInfo] = useState({
+    id: undefined,
+    name: '',
+    created_at: new Date(),
+    calories: 0,
+    price: 0,
+    user: {
+      id: undefined,
+      fullname: '',
+      username: '',
+      email: '',
+    },
+  })
+
+  useEffect(async () => {
+    if (typeof recordId === 'undefined') return
+
+    try {
+      const response = await axios.get(`/admin/${recordId}`)
+      const { record } = response.data
+
+      setRecordInfo(record)
+      setLoading(false)
+    } catch (err) {
+      console.error('Fetching Record Information Failed', {
+        err,
+      })
+    }
+  }, [recordId])
+
+  return (
+    <div className={commonStyles.container}>
+      <Space direction="vertical" style={{ width: '100%' }}>
+        {loading ? (
+          <div>
+            <InfoCircleTwoTone /> Loading Record Informaiton...
+          </div>
+        ) : (
+          <>
+            <Card>
+              <Title level={4}>
+                <UserOutlined /> Record Owner
+              </Title>
+              <p>
+                Full Name: <b>{recordInfo.user.fullname}</b>
+              </p>
+              <p>
+                User Name: <b>{recordInfo.user.username}</b>
+              </p>
+              <p>
+                Email: <b>{recordInfo.user.email}</b>
+              </p>
+            </Card>
+            <Card>
+              <Title level={4}>
+                <FolderOpenTwoTone /> Record Information
+              </Title>
+
+              <p>
+                Food Name: <b>{recordInfo.name}</b>
+              </p>
+              <p>
+                Food Calories: <b>{recordInfo.calories}</b>
+              </p>
+              <p>
+                Price: <b>{recordInfo.price}</b>
+              </p>
+              <p>
+                Recorded At:{' '}
+                <b>{new Date(recordInfo.created_at).toUTCString()}</b>
+              </p>
+            </Card>
+
+            <Space>
+              <Link
+                href={`/admin/users/${recordInfo.user.id}/records/${recordInfo.id}/edit`}
+              >
+                <Button icon={<EditTwoTone />}>Edit Record</Button>
+              </Link>
+              <Link
+                href={`/admin/users/${recordInfo.user.id}/records/${recordInfo.id}/delete`}
+              >
+                <Button icon={<DeleteTwoTone />}>Delete Record</Button>
+              </Link>
+            </Space>
+          </>
+        )}
+      </Space>
+    </div>
+  )
+}
+
+export default AdminRecordPage
